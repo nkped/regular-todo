ddbe27e69538daa933e7a8a2f565d5c915cc532b
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-11-13 15:30:57 +0100

    add css to addTodo

--- app/addTodo/page.js
+++ app/addTodo/page.js
@@ -20,20 +20,22 @@ export default function AddTodo() {
     //console.log('message from addTodo', res)
 
     return(
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form className="flex flex-col gap-3 px-4" onSubmit={(e) => handleSubmit(e)}>
             <input 
+                className="px-2 py-4 mt-8 rounded-md border border-slate-300 bg-slate-50 hover:placeholder-slate-300"
                 type="text" 
                 placeholder="Type title here.." 
                 value={title} 
                 onChange={(e) => setTitle(e.target.value)} 
                 />
             <input 
+                className="px-2 py-4 rounded-md border border-slate-300 bg-slate-50 hover:placeholder-slate-300"
                 type="text" 
                 placeholder="Type description here.." 
                 value={description} 
                 onChange={(e) => setDescription(e.target.value)}
                 />
-            <button type="submit">Add Todo</button>
+            <button className="py-4 mt-3 font-bold text-white bg-green-400 rounded-md border border-green-400 hover:shadow-md" type="submit">Add Todo</button>
         </form>
     )
 }
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,9 +4,9 @@ import Link from "next/link"
 export default function Navbar() {
 
     return(
-        <nav className="flex justify-between px-6 py-3" >
-            <Link href='/'>My Todos</Link>
-            <Link href='/addTodo'>Add Todo</Link>
+        <nav className="flex justify-between px-6 py-4 text-xl font-bold text-white hover:shadow-sm bg-slate-300" >
+            <Link className='hover:font-extrabold' href='/'>My Todos</Link>
+            <Link className='hover:font-extrabold' href='/addTodo'>Add Todo</Link>
         </nav>
     )
 }
