54728abfa6302ed44c70d10beb6ffad4277baa9a
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-11-14 11:20:46 +0100

    add update todo

--- app/components/EditTodo.js
+++ app/components/EditTodo.js
@@ -1,12 +1,56 @@
+'use client'
+import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 
+export default function EditTodo({ id, title, description }) {
 
-export default async function EditTodo({ id, title, description }) {
+    const [ newTitle, setNewTitle ] = useState(title)
+    const [ newDescription, setNewDescription ] = useState(description)
+    const router = useRouter()
 
     console.log('id, title, description from EditTodo', id, title, description)
 
+    const handleUpdate = async(e) => {
+        e.preventDefault()
+        console.log('newTitle, newDescription from EditTodo component', newTitle, newDescription)
+
+        try {
+            const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+                method: 'PUT', 
+                headers: {
+                    'Content-Type': 'application/json'
+                }, body: JSON.stringify({newTitle, newDescription})
+            })
+            if (res.ok) {
+                console.log('todo was updated')
+                router.push('/')
+                router.refresh()
+            }
+        } catch(err) {
+            console.log(err)
+        }
+
+        
+
+    }
+
     return (
-        <form><input placeholder="hello editTodo"/></form>
+        <form onSubmit={handleUpdate}>
+            <input 
+                type="text" 
+                placeholder={title} 
+                value={newTitle} 
+                onChange={(e) => setNewTitle(e.target.value)} 
+             />
+            <input 
+                type="text" 
+                placeholder={description} 
+                value={newDescription} 
+                onChange={(e) => setNewDescription(e.target.value)} 
+                />
+            <button>Update Todo</button>
+        </form>
     )
 
 
